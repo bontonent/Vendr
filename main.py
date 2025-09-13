b1f81a6d32117da82a82de7181f2f532bdff5bf6
bontonent <bontonent@tuta.io>
2025-09-13 10:37:05 +0300

    Initial commit(main preparation)

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Vendr:
+    def __init__(self,positions):
+        # create url element for work
+        self.url_positions = []
+        for position in positions:
+            position_text = position.replace(" ","-").lower()
+            self.url_positions.append(f"https://www.vendr.com/categories/{position_text}")
+
+    def main_directory(self):
+        # try work with him( maybe need User agent
+        # for start work only with
+        # https://www.vendr.com/categories/devops
+
+        print(self.url_positions)
+
+
+
+
+
+if __name__ == "__main__":
+    # we get work positions
+    positions = ["DevOps", "IT Infrastructure", "Data Analytics and Management"]
+    Vendr = Vendr(positions)
+    # start work with url element
+    Vendr.main_directory()
