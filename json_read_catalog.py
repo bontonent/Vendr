8636093b27150e57febc16b0bb4a40340b227b0a
bontonent <bontonent@tuta.io>
2025-09-15 22:43:33 +0300

    Finally commit

--- json_read_catalog.py
+++ json_read_catalog.py
@@ -1,13 +1,18 @@
+# Json for get all company code names
 import requests
 
 def json_read_cat(position,name_category):
+    # Element For create url
     re_name_category = name_category.replace("(", "").replace(")", "").replace("/", "").replace(" ", "-").replace(",","").lower()
     data_for_search = []
 
-    # !!!! dangerously. If None in page, I stop function (eazy find bot)
+    # In this site we can open page 3 from 2.
+    # If we open 3 without products.
+    # We don't get products and stop work with this url position
     i = 0
     while True:
         i = i + 1; page_data_for_search = []
+        # Connect to json with all necessary data
         url = f"https://www.vendr.com/categories/{position}/{re_name_category}?page={i}&_data=routes%2F_marketplace.categories.%24categorySlug.%24subCategorySlug._index"
         print(url)
         params = {
@@ -22,18 +27,20 @@ def json_read_cat(position,name_category):
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
         }
 
+        # if error try see response
         response = requests.get( url, headers=headers, params=params)
         json_work_space = response.json()
 
+        # Get data from json
         for js_companies in json_work_space["companies"]:
             slug = "".join(["https://www.vendr.com/marketplace/",js_companies["slug"]])
+
             print(slug)
 
             data_for_search.append(slug)
             page_data_for_search.append(slug)
 
-        # """data-disabled="true"""
         if len(page_data_for_search) == 0:
             break
-
+    # return all create url companies
     return data_for_search
